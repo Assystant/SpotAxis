2384ed37ba4e96dd767364d122640c66c2bbc22d
Laxmi Redekar <laxmiredekar07@gmail.com>
2025-03-19 18:42:25 +0530

    ugettext_lazy has been removed from Django 3+ version, Replace it with gettext_lazy

--- zinnia/apps.py
+++ zinnia/apps.py
@@ -1,7 +1,7 @@
 """Apps for Zinnia"""
 from __future__ import absolute_import
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class ZinniaConfig(AppConfig):
