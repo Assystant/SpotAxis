c8f0e2a1cf612d3e3b86b5d3b917f5fd04cc7fdd
Arjun T A <arjunta32@gmail.com>
2025-05-21 22:12:59 +0530

    completed DRF api for activities

--- TRM/urls.py
+++ TRM/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
 
     # API URLs
     url(r'^api/common/', include('common.api.urls')),
+    url(r'^api/activities/',include('activities.api.urls')),
 
     # Resources
     url(r'resources/comments/', include('django_comments.urls')),
