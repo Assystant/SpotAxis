162b60c402bf85ddab635aca275ce4c8094c92be
Arjun Thazhath <146722372+arjunthazhath2001@users.noreply.github.com>
2025-05-28 19:16:40 +0530

    added on-delete-params on one-to-one relationships (#130)

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1171,7 +1171,7 @@ class UserSettings(models.Model):
     We should always refer to user.usersettings.settings['setting_name'].
     """
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     settings_pickled = models.TextField(
         _('Settings Dictionary'),
