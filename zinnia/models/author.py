bcb4b8e730abeb0a87db2465c9bf4782e20db7e7
Arjun Thazhath <146722372+arjunthazhath2001@users.noreply.github.com>
2025-05-28 17:49:00 +0530

    #109 remove python2 unicode imports and decorator declarations (#124)
    * removed python2_unicode_compatible decorators and imports
    
    * removed python2_unicode_compatible decorators and imports

--- zinnia/models/author.py
+++ zinnia/models/author.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.apps import apps
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+
 
 from zinnia.managers import EntryRelatedPublishedManager
 from zinnia.managers import entries_published
@@ -28,7 +28,6 @@ class AuthorPublishedManager(models.Model):
         abstract = True
 
 
-@python_2_unicode_compatible
 class Author(safe_get_user_model(),
              AuthorPublishedManager):
     """
--- zinnia/models/category.py
+++ zinnia/models/category.py
@@ -1,7 +1,6 @@
 """Category model for Zinnia"""
 from __future__ import absolute_import
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import gettext_lazy as _
 
 from mptt.managers import TreeManager
@@ -12,7 +11,6 @@ from zinnia.managers import EntryRelatedPublishedManager
 from zinnia.managers import entries_published
 
 
-@python_2_unicode_compatible
 class Category(MPTTModel):
     """
     Simple model for categorizing entries.
