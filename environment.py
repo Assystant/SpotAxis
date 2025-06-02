b917e4b468db1a34fb04a08bf1dfb80f9353a911
Sai Bhaskar Kandula <137374965+SaiBhaskar0987@users.noreply.github.com>
2025-06-02 14:37:56 +0530

    Add files via upload
    Signed-off-by: Sai Bhaskar Kandula <137374965+SaiBhaskar0987@users.noreply.github.com>

---
+++ environment.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Development local
+ENVIRONMENT = 'local_development'
+
+# Develoment Server
+# ENVIRONMENT = 'server_development'
+
+# Production
+# ENVIRONMENT = 'productive'
