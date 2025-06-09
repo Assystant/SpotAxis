9aa17c64800ce7b79c92518a99727b74061b83f3
Shaunaksb <43343551+Shaunaksb@users.noreply.github.com>
2025-06-09 14:59:50 +0530

    moved environment varialbles to env file

--- TRM/settings_local_development.py
+++ TRM/settings_local_development.py
@@ -2,6 +2,10 @@
 
 from __future__ import absolute_import
 import os.path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -11,12 +15,12 @@ SITE_URL = 'http://spotaxis.com'
 SITE_SUFFIX = '.spotaxis.com/'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'TRM_local',                      # Or path to database file if using sqlite3.
-        'USER': 'TRM_user',                      # Not used with sqlite3.
-        'PASSWORD': 'pass',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': os.getenv('local_db_engine'),  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.getenv('local_db_name'),                      # Or path to database file if using sqlite3.
+        'USER': os.getenv('local_db_user'),                      # Not used with sqlite3.
+        'PASSWORD': os.getenv('local_db_password'),                  # Not used with sqlite3.
+        'HOST': os.getenv('local_db_host'),                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.getenv('local_db_port'),                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -44,25 +48,25 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_PATH, 'static'),
 ]
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'contact.travelder@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwerty123$'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv('local_email_host')
+EMAIL_HOST_USER = os.getenv('local_email_host_user')
+EMAIL_HOST_PASSWORD = os.getenv('local_email_host_password')
+EMAIL_PORT = os.getenv('local_email_port')
+EMAIL_USE_TLS = os.getenv('local_use_tls')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # TRM | localhost
-SOCIAL_AUTH_FACEBOOK_KEY = '886020148168119'
-SOCIAL_AUTH_FACEBOOK_SECRET = 'b7e32633ce8789a7ff072971cf12e303'
+SOCIAL_AUTH_FACEBOOK_KEY = os.getenv('local_facebooK_key')
+SOCIAL_AUTH_FACEBOOK_SECRET = os.getenv('local_facebook_secret')
 
 # TRM | sandbox
 # PAYPAL_MERCHANT_ID = 'DUGFVYW3LGYTW'
-PAYPAL_MERCHANT_ID = 'XKMWAANJEJQ5U'
-PAYPAL_ACCESS_TOKEN = 'access_token$sandbox$42fgg8mwznb89ys2$81fa0749fc14faafabffe99ea319375b'
-PAYPAL_CLIENT_ID = 'AawWwBbC0IUBhU4sJ8I3aPzjBXIjJyzcAXYNSXxC1JCO4l6K_kr5TIU085cjwmOq34mg4pzCFyLkZeeK'
-PAYPAL_APP_SECRET = 'EGkHxjwddqKnIDr6LUEPyY2IVqygyZxbf27M3RNhLUMEi1jUG4Wl864ZUfBHu0uM6V6fPjT_k5aSbgEG'
-PAYPAL_ACCOUNT = 'contact@travelder.com'
+PAYPAL_MERCHANT_ID = os.getenv('local_paypal_merchant_id')
+PAYPAL_ACCESS_TOKEN = os.getenv('local_paypal_access_token')
+PAYPAL_CLIENT_ID = os.getenv('local_paypal_client_id')
+PAYPAL_APP_SECRET = os.getenv('local_paypal_app_secret')
+PAYPAL_ACCOUNT = os.getenv('local_paypal_account')
 
 import paypalrestsdk
 paypalrestsdk.configure({
@@ -87,26 +91,26 @@ paypalrestsdk.configure({
 # FACEBOOK_APP_ID = '1474071052897625'
 # FACEBOOK_APP_SECRET = 'bdac3b3fefdbd129bd7de7974f33bfc0'
 
-SOCIALAUTH_FACEBOOK_OAUTH_KEY = '745678155587072'
-SOCIALAUTH_FACEBOOK_OAUTH_SECRET = '6fc2442d9a23bcfc104bb4fc27d5ccc1'
+SOCIALAUTH_FACEBOOK_OAUTH_KEY = os.getenv('local_facebook_oauth_key')
+SOCIALAUTH_FACEBOOK_OAUTH_SECRET = os.getenv('local_facebook_oauth_secret')
 
-SOCIALAUTH_LINKEDIN_OAUTH_KEY = '81m81bg9hnd3jt'
-SOCIALAUTH_LINKEDIN_OAUTH_SECRET = '3S8YXZEOIllmDsef'
+SOCIALAUTH_LINKEDIN_OAUTH_KEY = os.getenv('local_linkedin_oauth_key')
+SOCIALAUTH_LINKEDIN_OAUTH_SECRET = os.getenv('local_linkedin_oauth_secret')
 
-SOCIALAUTH_ANGEL_OAUTH_KEY = ''
-SOCIALAUTH_ANGEL_OAUTH_SECRET = ''
+SOCIALAUTH_ANGEL_OAUTH_KEY = os.getenv('local_angel_oauth_key')
+SOCIALAUTH_ANGEL_OAUTH_SECRET = os.getenv('local_angel_oauth_secret')
 
-SOCIALAUTH_TWITTER_OAUTH_KEY = 'C6Jqc9i9Pr0whIcU18Fz8CNMQ'
-SOCIALAUTH_TWITTER_OAUTH_SECRET = 'hNA7Xqf2gTXdUFmYQbvVXZOoM1HzeQn6PR1kduWMDXcdqvRdD3'
+SOCIALAUTH_TWITTER_OAUTH_KEY = os.getenv('local_twitter_oauth_key')
+SOCIALAUTH_TWITTER_OAUTH_SECRET = os.getenv('local_twitter_oauth_secret')
 
-SOCIALAUTH_GOOGLEPLUS_OAUTH_KEY = '802095291346-o5eh8729ti70t1bbaj3h31qhmi99qitu.apps.googleusercontent.com'
-SOCIALAUTH_GOOGLEPLUS_OAUTH_SECRET = 'ZrIkLhkTQ5ce84_geu6HxmGo'
+SOCIALAUTH_GOOGLEPLUS_OAUTH_KEY = os.getenv('local_googleplus_oauth_key')
+SOCIALAUTH_GOOGLEPLUS_OAUTH_SECRET = os.getenv('local_googleplus_oauth_secret')
 
-SOCIALAUTH_GITHUB_OAUTH_KEY = 'fb93387bbc9f9ffcd2f7'
-SOCIALAUTH_GITHUB_OAUTH_SECRET = '9e17b019a9d6fb3b6b843a4d462006ffb8d0dfa0'
+SOCIALAUTH_GITHUB_OAUTH_KEY = os.getenv('local_github_oauth_key')
+SOCIALAUTH_GITHUB_OAUTH_SECRET = os.getenv('local_github_oauth_secret')
 
-SOCIALAUTH_STACKOVERFLOW_OAUTH_KEY = '8932'
-SOCIALAUTH_STACKOVERFLOW_OAUTH_SECRET = 'qREi*HWy7c6xpMlnhSqjiQ(('
-SOCIALAUTH_STACKOVERFLOW_OAUTH_REQUESTKEY = 'XDR1z6q3cmTZlOA**gbjPA(('
+SOCIALAUTH_STACKOVERFLOW_OAUTH_KEY = os.getenv('local_stackoverflow_oauth_key')
+SOCIALAUTH_STACKOVERFLOW_OAUTH_SECRET = os.getenv('local_stackoverflow_oauth_secret')
+SOCIALAUTH_STACKOVERFLOW_OAUTH_REQUESTKEY = os.getenv('local_stackoverflow_oatuh_requestkey')
 
 logo_email = 'http://spotaxis.com/static/img/logo/logo.png'
--- TRM/settings_server_development.py
+++ TRM/settings_server_development.py
@@ -2,6 +2,10 @@
 
 from __future__ import absolute_import
 import os.path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
 protocol = 'http'
@@ -10,12 +14,12 @@ SITE_SUFFIX = '.demo.spotaxis.com'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'TRM_deployment_test',                      # Or path to database file if using sqlite3.
-        'USER': 'TRM_user',                      # Not used with sqlite3.
-        'PASSWORD': 'TRM_db_password',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': os.getenv('server_dev_db_engine'),  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.getenv('server_dev_db_name'),                      # Or path to database file if using sqlite3.
+        'USER': os.getenv('server_dev_db_user'),                      # Not used with sqlite3.
+        'PASSWORD': os.getenv('server_dev_db_password'),                  # Not used with sqlite3.
+        'HOST': os.getenv('server_dev_db_host'),                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.getenv('server_dev_db_port'),                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 SESSION_COOKIE_DOMAIN = '.example.com'
@@ -42,14 +46,14 @@ STATICFILES_DIRS = [
     # Don't forget to use absolute paths, not relative paths.
     '/var/www/demo/trm/TRM/static/',
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL='SpotAxis <no-reply@spotaxis.com>'
-SERVER_EMAIL = 'no-reply@spotaxis.com'
+EMAIL_BACKEND = os.getenv('server_dev_email_backend') 
+EMAIL_HOST = os.getenv('server_dev_email_host')
+EMAIL_PORT = os.getenv('server_dev_email_port')
+EMAIL_HOST_USER = os.getenv('server_dev_email_host_user')
+EMAIL_HOST_PASSWORD = os.getenv('server_dev_email_password')
+EMAIL_USE_TLS = os.getenv('server_dev_email_use_tls')
+DEFAULT_FROM_EMAIL= os.getenv('sever_dev_default_from_email')
+SERVER_EMAIL = os.getenv('sever_dev_server_email')
 # EMAIL_HOST = 'email-smtp.us-west-2.amazonaws.com'
 # EMAIL_HOST_USER = 'AKIAJJLTCMM766VE2HEQ'
 # EMAIL_HOST_PASSWORD = 'AgxGkP/sAd6Z5pKCCJQblZF9ZEE+VbS+GdP09vTbnON1'
@@ -69,11 +73,11 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
-PAYPAL_MERCHANT_ID = 'XKMWAANJEJQ5U'
-PAYPAL_ACCESS_TOKEN = 'access_token$sandbox$42fgg8mwznb89ys2$81fa0749fc14faafabffe99ea319375b'
-PAYPAL_CLIENT_ID = 'AawWwBbC0IUBhU4sJ8I3aPzjBXIjJyzcAXYNSXxC1JCO4l6K_kr5TIU085cjwmOq34mg4pzCFyLkZeeK'
-PAYPAL_APP_SECRET = 'EGkHxjwddqKnIDr6LUEPyY2IVqygyZxbf27M3RNhLUMEi1jUG4Wl864ZUfBHu0uM6V6fPjT_k5aSbgEG'
-PAYPAL_ACCOUNT = 'contact@travelder.com'
+PAYPAL_MERCHANT_ID = os.getenv('server_dev_paypal_merchant_id')
+PAYPAL_ACCESS_TOKEN = os.getenv('server_dev_paypal_access_token')
+PAYPAL_CLIENT_ID = os.getenv('server_dev_paypal_client_id')
+PAYPAL_APP_SECRET = os.getenv('server_dev_paypal_app_secret')
+PAYPAL_ACCOUNT = os.getenv('server_dev_paypal_account')
 
 import paypalrestsdk
 paypalrestsdk.configure({
@@ -83,24 +87,24 @@ paypalrestsdk.configure({
     })
 
 
-SOCIALAUTH_FACEBOOK_OAUTH_KEY = '745678155587072'
-SOCIALAUTH_FACEBOOK_OAUTH_SECRET = '6fc2442d9a23bcfc104bb4fc27d5ccc1'
+SOCIALAUTH_FACEBOOK_OAUTH_KEY = os.getenv('server_dev_facebook_oauth_key')
+SOCIALAUTH_FACEBOOK_OAUTH_SECRET = ('server_dev_facebook_oauth_secret')
 
-SOCIALAUTH_LINKEDIN_OAUTH_KEY = '81m81bg9hnd3jt'
-SOCIALAUTH_LINKEDIN_OAUTH_SECRET = '3S8YXZEOIllmDsef'
+SOCIALAUTH_LINKEDIN_OAUTH_KEY = ('server_dev_linkedin_oauth_key')
+SOCIALAUTH_LINKEDIN_OAUTH_SECRET = ('server_dev_linkedin_oauth_secret')
 
-SOCIALAUTH_ANGEL_OAUTH_KEY = ''
-SOCIALAUTH_ANGEL_OAUTH_SECRET = ''
+SOCIALAUTH_ANGEL_OAUTH_KEY = ('server_dev_angel_oauth_key')
+SOCIALAUTH_ANGEL_OAUTH_SECRET = ('server_dev_angel_oauth_secret')
 
-SOCIALAUTH_TWITTER_OAUTH_KEY = 'C6Jqc9i9Pr0whIcU18Fz8CNMQ'
-SOCIALAUTH_TWITTER_OAUTH_SECRET = 'hNA7Xqf2gTXdUFmYQbvVXZOoM1HzeQn6PR1kduWMDXcdqvRdD3'
+SOCIALAUTH_TWITTER_OAUTH_KEY = ('server_dev_twitter_oauth_key')
+SOCIALAUTH_TWITTER_OAUTH_SECRET = ('server_dev_twitter_oauth_secret')
 
-SOCIALAUTH_GOOGLEPLUS_OAUTH_KEY = '802095291346-o5eh8729ti70t1bbaj3h31qhmi99qitu.apps.googleusercontent.com'
-SOCIALAUTH_GOOGLEPLUS_OAUTH_SECRET = 'ZrIkLhkTQ5ce84_geu6HxmGo'
+SOCIALAUTH_GOOGLEPLUS_OAUTH_KEY = ('server_dev_googleplus_oauth_key')
+SOCIALAUTH_GOOGLEPLUS_OAUTH_SECRET = ('server_dev_googleplus_oauth_secret')
 
-SOCIALAUTH_GITHUB_OAUTH_KEY = 'fb93387bbc9f9ffcd2f7'
-SOCIALAUTH_GITHUB_OAUTH_SECRET = '9e17b019a9d6fb3b6b843a4d462006ffb8d0dfa0'
+SOCIALAUTH_GITHUB_OAUTH_KEY = ('server_dev_github_oauth_key')
+SOCIALAUTH_GITHUB_OAUTH_SECRET = ('server_dev_github_oauth_secret')
 
-SOCIALAUTH_STACKOVERFLOW_OAUTH_KEY = '8932'
-SOCIALAUTH_STACKOVERFLOW_OAUTH_SECRET = 'qREi*HWy7c6xpMlnhSqjiQ(('
-SOCIALAUTH_STACKOVERFLOW_OAUTH_REQUESTKEY = 'XDR1z6q3cmTZlOA**gbjPA(('
+SOCIALAUTH_STACKOVERFLOW_OAUTH_KEY = ('server_dev_stackoverflow_oauth_key')
+SOCIALAUTH_STACKOVERFLOW_OAUTH_SECRET = ('server_dev_stackoverflow_oauth_secret')
+SOCIALAUTH_STACKOVERFLOW_OAUTH_REQUESTKEY = ('server_dev_stackoverflow_oauth_requestkey')
