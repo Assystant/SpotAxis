9d32c099561d90062870f9e9550fb80c7e3c16d2
Sai Bhaskar Kandula <137374965+SaiBhaskar0987@users.noreply.github.com>
2025-08-13 18:02:05 +0530

    removed () (#301)
    Co-authored-by: Saket Shukla <invincible.shukla@gmail.com>

--- common/ajax.py
+++ common/ajax.py
@@ -866,7 +866,7 @@ def validate_personal_form(request):
     context['success'] = False
     context['post'] = request.POST
     if is_ajax(request) and request.method == 'POST':
-        if request.user.is_anonymous() and request.session.get('active_applicant'):
+        if request.user.is_anonymous and request.session.get('active_applicant'):
             try:
                 candidate = [SocialAuth.objects.get(id=int(request.session['active_applicant'])).user.candidate]
             except:
